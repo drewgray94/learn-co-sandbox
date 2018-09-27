9d4e9c415d8b344853514240eb77fa645f2e03bc
Drew Gray <drew.gray@havasmedia.com>
2018-09-27 21:26:10 +0000

    automatically backed up by learn

---
+++ day_2_exercises.py
@@ -0,0 +1,15 @@
+# 1. Grab the number 6 from this nested_list and set it equal to the number_six variable
+
+nested_list = [1, [2, [4, [5, [6]], 3]]]
+
+number_six = nested_list[1][1][1][0]
+
+number_six
+
+# 2. Sort the cats list in alphabetical order
+
+cats = ['Maine Coon', 'Tabby', 'Siamese', 'Garfield', 'Sylvester']
+
+sorted(cats)
+
+cats
